0233487f7e3cc86f094335a0de54fed2f53cd923
ayushisahoo22 <ayushiis0912@gmail.com>
2025-09-04 00:23:08 +0530

    Added rotate methods upto k element

--- array.cpp/right_rotate.cpp
+++ array.cpp/right_rotate.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main(){
     int arr[100];
     int n;
@@ -12,18 +13,22 @@ int main(){
     cout<<"Enter the value of k: ";
     cin>>k;
     k=k%n;
+    //storing values in temp
     int temp[100];
-    for(int i=0;i<k;i++){
-        temp[i]=arr[i];
-    }
-    for(int i=k;i<n;i++){
-        arr[i-k]=arr[i];
-    }
     int j=0;
     for(int i=n-k;i<n;i++){
-        arr[i]=temp[j];
+        temp[j]=arr[i];
         j++;
     }
+    //shifting of the array
+    for(int i=n-1;i>=k;i--){
+        arr[i]=arr[i-k];
+    }
+    //merging temp to arr
+    for(int i=0;i<k;i++){
+        arr[i]=temp[i];
+    }
+    //displaying values
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
