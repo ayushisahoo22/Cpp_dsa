245fbf5456bc9f500f772297ef2dcd327bedd25a
ayushisahoo22 <ayushiis0912@gmail.com>
2025-09-18 00:48:21 +0530

    Longest subsequence by not sorting the array

--- array_med.cpp/consecutive_seq.cpp
+++ array_med.cpp/consecutive_seq.cpp
@@ -8,8 +8,8 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    //distorting the array
-    sort(arr.begin(),arr.end());
+    //distorting the array  ---> O(NlogN)
+    /*sort(arr.begin(),arr.end());
     int count=0,longest=1;
     int last_small=INT_MIN;
     for(int i=0;i<n;i++){
@@ -21,6 +21,26 @@ int main(){
             last_small=arr[i];
         }
         longest=max(longest,count);
+    }
+    cout<<longest;*/
+
+    //by not sorting   TC---> O(3N)
+    //                 SC---> O(N) 
+    int longest=1;
+    unordered_set<int> st;
+    for(int i=0;i<n;i++){
+        st.insert(arr[i]);
+    }
+    for(auto it:st){
+        if(st.find(it-1)==st.end()){
+            int cnt=1;
+            int x=it;
+            while(st.find(x+1) != st.end()){
+                cnt+=1;
+                x+=1;
+            }
+            longest=max(longest,cnt);
+        }
     }
     cout<<longest;
 }
