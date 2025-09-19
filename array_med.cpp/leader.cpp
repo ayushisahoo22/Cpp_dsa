99dafcade092eaa6e8c98a6b0ae635bfe1056e47
ayushisahoo22 <ayushiis0912@gmail.com>
2025-09-19 11:03:05 +0530

    leader question

--- array_med.cpp/leader.cpp
+++ array_med.cpp/leader.cpp
@@ -1,20 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-//Last element will always be the leader
-//The element will be leader when every right elements to it are smaller 
-int main(){
+// Last element will always be the leader
+// The element will be leader when every right elements to it are smaller
+int main()
+{
     int n;
-    cout<<"Enter the size of the vector ";
-    cin>>n;
+    cout << "Enter the size of the vector ";
+    cin >> n;
     vector<int> arr(n);
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
 
     vector<int> ans;
 
-    //TC--->O(N^2)
+    // TC--->O(N^2)
     /*for(int i=0;i<n;i++){
         bool isLead=true;
         for(int j=i+1;j<n;j++){
@@ -28,17 +30,20 @@ int main(){
         }
     }*/
 
-    //TC--->O(N)
-    int max=arr[n-1];
+    // TC--->O(N)
+    int max = arr[n - 1];
     ans.push_back(max);
-    for(int i=n-2;i>=0;i--){
-        if(arr[i]>max){
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] > max)
+        {
             ans.push_back(arr[i]);
-            max=arr[i];
+            max = arr[i];
         }
     }
-    reverse(ans.begin(),ans.end());
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
+    reverse(ans.begin(), ans.end());
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
     }
 }
