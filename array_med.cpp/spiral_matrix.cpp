4b47136eaf938837aebdb25cc042f57afe8a1447
ayushisahoo22 <ayushiis0912@gmail.com>
2025-09-22 23:37:15 +0530

    Printing matrix in spiral

--- array_med.cpp/spiral_matrix.cpp
+++ array_med.cpp/spiral_matrix.cpp
@@ -13,23 +13,28 @@ int main(){
         }
         cout << endl;
     }
-    //right
     int top=0 ,bottom=n-1,left=0,right=n-1;
     while(left<=right && top<=bottom){
+        //right
         for(int i=left;i<=right;i++){
             cout<<arr[top][i]<<" ";
         }
         top++;
+        //bottom
         for(int i=top;i<=bottom;i++){
             cout<<arr[i][right]<<" ";
         }
         right--;
+        //left
+        //if is added for edge case if there is only one row in array
         if(top<=bottom){
             for(int i=right;i>=left;i--){
                 cout<<arr[bottom][i]<<" ";
             }
             bottom--;
         }
+        //top
+        //if is added for edge case if there is only one column in array
         if(left<=right){
             for(int i=bottom;i>=top;i--){
                 cout<<arr[i][left]<<" ";
