553f21cf664a7fa74d0c0dfc95afd0248ddf049a
ayushisahoo22 <ayushiis0912@gmail.com>
2025-09-25 14:00:06 +0530

    Printing number of pascal

---
+++ array_hard.cpp/pascal.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    //to print a specific value at [n][r]
+    /*int n,r;
+    cin>>n>>r;
+    int res=1;
+    for(int i=1;i<r;i++){
+        res=res*(n-i);
+        res=res/(i);
+    }  
+    cout<<res<<endl;*/
+
+    
+}
