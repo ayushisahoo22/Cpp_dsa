fde7d931ba9695964678cd8075e76dba8be0abb5
ayushisahoo22 <ayushiis0912@gmail.com>
2025-09-26 23:44:14 +0530

    Moore's Voting algo-II

--- array_hard.cpp/majority_3.cpp
+++ array_hard.cpp/majority_3.cpp
@@ -8,7 +8,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    vector<int> ans;
+    /*vector<int> ans;
     map<int,int>mpp;
     int mini=(n/3)+1;
     for(int i=0;i<n;i++){
@@ -26,6 +26,42 @@ int main(){
     }
     for(int i=0;i<ans.size();i++){
         cout<<ans[i]<<" ";
+    }*/
+
+
+    //MOORE'S VOTING ALGO-II
+    vector<int> ans;
+    int cnt1=0,cnt2=0;
+    int ele1=INT_MIN,ele2=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(cnt1==0 && ele2!=arr[i]){
+            ele1=arr[i];
+            cnt1=1;
+        }else if(cnt2==0 && ele1!=arr[i]){
+            ele2=arr[i];
+            cnt2=1;
+        }else if(arr[i]==ele1){
+            cnt1++;
+        }else if(arr[i]==ele2){
+            cnt2++;
+        }else{
+            cnt1--;
+            cnt2--;
+        }
     }
-    
+    //TO MANUALLY CHECK
+    cnt1=0,cnt2=0;
+    for(int i=0;i<n;i++){
+        if(ele1==arr[i]){cnt1++;}
+        if(ele2==arr[i]){cnt2++;}
+    }
+    int mini=(n/3)+1;
+    if(cnt1>=mini){
+        ans.push_back(ele1);
+    }if(cnt2>=mini){
+        ans.push_back(ele2);
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
 }
