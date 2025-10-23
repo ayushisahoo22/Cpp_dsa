77bec467ef4a3e3fb782de10806dbcecaa414fa2
ayushisahoo22 <ayushiis0912@gmail.com>
2025-10-23 13:10:30 +0530

    Optimal for three sum

--- array_hard.cpp/3Sum.cpp
+++ array_hard.cpp/3Sum.cpp
@@ -8,7 +8,7 @@ using namespace std;
 //                  SC-->O(N)+2*O(Number of ele)
 
 //TLE occur
-vector<vector<int>> threeSum(vector<int>& nums) {
+/*vector<vector<int>> threeSum(vector<int>& nums) {
     set<vector<int>>st;
     for(int i=0;i<nums.size();i++){
         set<int>val;
@@ -23,6 +23,42 @@ vector<vector<int>> threeSum(vector<int>& nums) {
         }
     }
     vector<vector<int>>ans(st.begin(),st.end());
+    return ans;
+}*/
+
+//Optimal approach TC-->O(NlogN)+O(N^N)
+//                 SC-->O(No of unique triplets)
+
+//eg -->[-2,-2,-2,-2,-1,-1,-1,0,0,0,2,2,2,2]
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>>ans;
+    sort(nums.begin(),nums.end());   //sorting the array
+    for(int i=0;i<nums.size();i++){
+
+        //if i is greater than 0 and equal to previous one skip that 
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        int j=i+1;  
+        int k=nums.size()-1;
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum<0){  //if sum is less than 0 we will increase j to be near to 0
+                j++;
+            }else if(sum>0){
+                k--;   //if sum is greater than 0 we will decrease k to be near to 0
+            }else{
+                vector<int>temp={nums[i],nums[j],nums[k]};
+                ans.push_back(temp);
+                //after storing particular i,j,k..increase j k for next triplet
+                j++;
+                k--;
+
+                //if the j and k are same till a point skip that part using while
+                //using j<k so it won't run endless time
+                while(j<k && nums[j]==nums[j-1]) j++;
+                while(j<k && nums[k]==nums[k+1]) k--;
+            }
+        }
+    }
     return ans;
 }
 
