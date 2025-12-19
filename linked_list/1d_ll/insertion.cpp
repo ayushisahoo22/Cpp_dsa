c6bdf370501e359a37d8b2d9d2aab7e7f170befb
ayushisahoo22 <ayushiis0912@gmail.com>
2025-12-19 12:20:22 +0530

    Inserting at head of 1D LL

---
+++ linked_list/1d_ll/insertion.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+
+    //constructor
+    Node(int data1,Node *next1){
+        data=data1;
+        next=next1;
+    }
+
+    //constructor
+    Node(int data1){
+        data= data1;
+        next= nullptr;
+    }
+};
+
+Node *convertArr2LL(vector<int>arr){
+    Node *head=new Node(arr[0]);
+    Node *mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node *temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=mover->next;
+    }
+    return head;
+}
+
+void print(Node *head){
+    Node *temp=head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+Node *insertHead(Node *head,int val){
+    Node *temp=new Node(val,head);
+    head=temp;
+    return head;
+}
+
+int main(){
+    vector<int> arr={7,3,2,4,5};
+    Node *head=convertArr2LL(arr);
+    int val;
+    cout<<"Enter a value: ";
+    cin>>val;
+    head=insertHead(head,val);
+    print(head);
+}
