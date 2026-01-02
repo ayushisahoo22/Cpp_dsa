843c65ff65e6ec7de7cdeb0f13349d99fab80ed2
ayushisahoo22 <ayushiis0912@gmail.com>
2026-01-02 22:24:06 +0530

    Lower bound in 1D array

---
+++ binary_search/1d_array/lower_bound.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// The lower bound is the smallest index, ind, where arr[ind] >= x
+// if any such index is not found the lower bound algorithm returns n i.e. size of the given array.
+
+int lb(vector<int>arr,int x){
+    int n=arr.size();
+    int low=0,high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=(high+low)/2;
+        if(arr[mid]>=x){
+            ans=mid;
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+//direct
+//lb=lower_bound(arr.begin(),arr.end(),x);
+
+int main(){
+    int n,k;
+    cout<<"Enter n and x: ";
+    cin>>n>>k;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    int ans=lb(arr,k);
+    cout<<ans;
+}
