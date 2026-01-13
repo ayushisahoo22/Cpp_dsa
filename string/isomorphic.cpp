9c6406464437868b4b7e1132886134ea48f9c2c6
ayushisahoo22 <ayushiis0912@gmail.com>
2026-01-13 23:14:48 +0530

    Isomorphic strings check

---
+++ string/isomorphic.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//check if strings are isomorphic
+
+class Solution {
+public:
+    bool isomorphicString(string s, string t) {
+        // Arrays to store last seen positions of characters in s and t
+        int m1[256] = {0}, m2[256] = {0}; 
+        
+        // Get the length of the strings
+        int n = s.size(); 
+        
+        // Traverse each character of the strings
+        for (int i = 0; i < n; ++i) {
+            // If previous positions of current characters differ, return false
+            if (m1[s[i]] != m2[t[i]]) return false;
+            
+            // Update the position with current index + 1
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution solution;
+    string s = "paper";
+    string t = "title";
+    
+    // Check if strings are isomorphic
+    if (solution.isomorphicString(s, t)) {
+        cout << "Strings are isomorphic." << endl;
+    } else {
+        cout << "Strings are not isomorphic." << endl;
+    }
+    
+    return 0;
+}
