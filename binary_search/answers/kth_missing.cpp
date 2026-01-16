a11c22493d1cb5e394695aa23f5bb7b171c30ce3
ayushisahoo22 <ayushiis0912@gmail.com>
2026-01-16 23:31:55 +0530

    missing number in sorted array using BS

--- binary_search/answers/kth_missing.cpp
+++ binary_search/answers/kth_missing.cpp
@@ -5,18 +5,46 @@ using namespace std;
 
 //tc->O(N)
 
+// class MissingKFinder {
+// public:
+//     // Function to find the k-th missing number
+//     int missingK(vector<int> vec, int n, int k) {
+//         for (int i = 0; i < n; i++) {
+//             if (vec[i] <= k) {
+//                 k++;  // If current number is less than or equal to k, increment k
+//             } else {
+//                 break; // Stop when we reach a number greater than k
+//             }
+//         }
+//         return k;  // Return the final value of k which is the missing number
+//     }
+// };
+
+//tc->O(logN)
+
 class MissingKFinder {
 public:
-    // Function to find the k-th missing number
+    // Function to return the k-th missing number
     int missingK(vector<int> vec, int n, int k) {
-        for (int i = 0; i < n; i++) {
-            if (vec[i] <= k) {
-                k++;  // If current number is less than or equal to k, increment k
+        int low = 0, high = n - 1;
+
+        // Perform binary search
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // Calculate how many numbers are missing till vec[mid]
+            int missing = vec[mid] - (mid + 1);
+
+            if (missing < k) {
+                low = mid + 1;  // Move right to find more missing numbers
             } else {
-                break; // Stop when we reach a number greater than k
+                high = mid - 1; // Move left to find a smaller valid index
             }
         }
-        return k;  // Return the final value of k which is the missing number
+
+        // After loop, 'high' points to the largest index such that
+        // number of missing elements till there < k
+        return k + high + 1;
     }
 };
 
