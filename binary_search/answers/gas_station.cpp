37c74c7376914eaa52a2796d8c2b56b5506628ce
ayushisahoo22 <ayushiis0912@gmail.com>
2026-01-27 23:38:13 +0530

    gas station prblm using binary search

--- binary_search/answers/gas_station.cpp
+++ binary_search/answers/gas_station.cpp
@@ -7,23 +7,52 @@ using namespace std;
 // Let 'dist' be the maximum value of the distance between adjacent gas stations after adding k new gas stations.Find the minimum value of ‘dist’.
 
 //better approach
-long double minDistance(vector<int> &arr,int k){
+// long double minDistance(vector<int> &arr,int k){
+//     int n=arr.size();
+//     vector<int>howmany(n-1,0);
+//     priority_queue<pair<long double,int>>pq;
+//     for(int i=0;i<n-1;i++){
+//         pq.push({arr[i+1]-arr[i],i});
+//     }
+//     for(int i=1;i<=k;i++){
+//         auto tp=pq.top();
+//         pq.pop();
+//         int segmentInd=tp.second;
+//         howmany[segmentInd]++;
+//         long double totalDist=arr[segmentInd+1]-arr[segmentInd];
+//         long double newLen=totalDist/(howmany[segmentInd]+1);
+//         pq.push({newLen,segmentInd});
+//     }
+//     return pq.top().first;
+// }
+
+int numberOfGasStat(vector<int>arr,long double dist){
     int n=arr.size();
-    vector<int>howmany(n-1,0);
-    priority_queue<pair<long double,int>>pq;
+    int cnt=0;
+    for(int i=1;i<n;i++){
+        int no=(arr[i]-arr[i-1])/dist;
+        if((arr[i]-arr[i-1])== no*dist){
+            no--;
+        }
+        cnt+=no;
+    }
+    return cnt;
+}
+
+long double minDistance(vector<int>&arr , int k){
+    int n=arr.size();
+    long double low=0,high=0;
     for(int i=0;i<n-1;i++){
-        pq.push({arr[i+1]-arr[i],i});
+        high=max(high,(long double)arr[i+1]-arr[i]);
     }
-    for(int i=1;i<=k;i++){
-        auto tp=pq.top();
-        pq.pop();
-        int segmentInd=tp.second;
-        howmany[segmentInd]++;
-        long double totalDist=arr[segmentInd+1]-arr[segmentInd];
-        long double newLen=totalDist/(howmany[segmentInd]+1);
-        pq.push({newLen,segmentInd});
+    long double diff=1e-6;
+    while(high-low>diff){
+        long double mid=(low+high)/2.0;
+        int n=numberOfGasStat(arr,mid);
+        if(n>k) low=mid;
+        else high=mid;
     }
-    return pq.top().first;
+    return high;
 }
 
 int main(){
