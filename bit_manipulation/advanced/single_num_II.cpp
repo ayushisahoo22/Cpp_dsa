3175cfa0333d9299153a75ff6a1a4cdf841408ed
ayushisahoo22 <ayushiis0912@gmail.com>
2026-02-10 21:03:10 +0530

    Find single no (optimal)

--- bit_manipulation/advanced/single_num_II.cpp
+++ bit_manipulation/advanced/single_num_II.cpp
@@ -4,15 +4,25 @@ using namespace std;
 // Given an integer array nums where every element appears three times except for one,
 // which appears exactly once. Find the single element and return it.
 
+// int findMissing(vector<int>arr){
+//     int n=arr.size();
+//     sort(arr.begin(),arr.end());
+//     for(int i=1;i<n;i=i+3){
+//         if(arr[i]!=arr[i-1]){
+//             return arr[i-1];
+//         }
+//     }
+//     return arr[n-1];
+// }
+
+//TC->O(N) 
 int findMissing(vector<int>arr){
-    int n=arr.size();
-    sort(arr.begin(),arr.end());
-    for(int i=1;i<n;i=i+3){
-        if(arr[i]!=arr[i-1]){
-            return arr[i-1];
-        }
+    int ones=0,twos=0;
+    for(int i=0;i<arr.size();i++){
+        ones=(ones^arr[i])& (~twos);
+        twos=(twos^arr[i])& (~ones);
     }
-    return arr[n-1];
+    return ones;
 }
 
 int main(){
