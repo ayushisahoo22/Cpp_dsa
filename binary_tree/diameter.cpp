4c496d14929a61efe766a1ed2b396174c3847c18
ayushisahoo22 <ayushiis0912@gmail.com>
2026-04-01 23:45:17 +0530

    Diameter of Binary Tree

---
+++ binary_tree/diameter.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// Node structure for the binary tree
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+int height(Node* node, int& diameter) {
+    if (!node) {
+        return 0;
+    }
+    int lh = height(node->left, diameter);
+    int rh = height(node->right, diameter);
+    diameter = max(diameter, lh + rh);
+    return 1 + max(lh, rh);
+}
+
+int diameterOfBinaryTree(Node* root) {
+    int diameter = 0;
+    height(root, diameter);
+    return diameter;
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->right = new Node(6);
+    root->left->right->right->right = new Node(7);
+    int diameter =diameterOfBinaryTree(root);
+    cout << "The diameter of the binary tree is: " << diameter << endl;
+    return 0;
+}
