d2a75e32541e1d4db944769b89614c816c1a723f
ayushisahoo22 <ayushiis0912@gmail.com>
2026-06-04 21:24:17 +0530

    Boundary traversal of tree

---
+++ binary_tree/boundary_traversal.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    bool isLeaf(Node* root) {
+        return !root->left && !root->right;
+    }
+    void addLeftBoundary(Node* root, vector<int>& res) {
+        Node* curr = root->left;
+        while (curr) {
+            // If the current node is not a leaf,
+            // add its value to the result
+            if (!isLeaf(curr)) {
+                res.push_back(curr->data);
+            }
+            // Move to the left child if it exists,
+            // otherwise move to the right child
+            if (curr->left) {
+                curr = curr->left;
+            } else {
+                curr = curr->right;
+            }
+        }
+    }
+    void addRightBoundary(Node* root, vector<int>& res) {
+        Node* curr = root->right;
+        vector<int> temp;
+        while (curr) {
+            if (!isLeaf(curr)) {
+                temp.push_back(curr->data);
+            }
+            // Move to the right child if it exists,
+            // otherwise move to the left child
+            if (curr->right) {
+                curr = curr->right;
+            } else {
+                curr = curr->left;
+            }
+        }
+        // Reverse and add the values from
+        // the temporary vector to the result
+        for (int i = temp.size() - 1; i >= 0; --i) {
+            res.push_back(temp[i]);
+        }
+    }
+    void addLeaves(Node* root, vector<int>& res) {
+        // If the current node is a
+        // leaf, add its value to the result
+        if (isLeaf(root)) {
+            res.push_back(root->data);
+            return;
+        }
+        // Recursively add leaves of
+        // the left and right subtrees
+        if (root->left) {
+            addLeaves(root->left, res);
+        }
+        if (root->right) {
+            addLeaves(root->right, res);
+        }
+    }
+    vector<int> printBoundary(Node* root) {
+        vector<int> res;
+        if (!root) {
+            return res;
+        }
+        // If the root is not a leaf,
+        // add its value to the result
+        if (!isLeaf(root)) {
+            res.push_back(root->data);
+        }
+
+        // Add the left boundary, leaves,
+        // and right boundary in order
+        addLeftBoundary(root, res);
+        addLeaves(root, res);
+        addRightBoundary(root, res);
+
+        return res;
+    }
+};
+
+void printResult(const vector<int>& result) {
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    Solution solution;
+    vector<int> result = solution.printBoundary(root);
+    cout << "Boundary Traversal: ";
+    printResult(result);
+
+    return 0;
+}
