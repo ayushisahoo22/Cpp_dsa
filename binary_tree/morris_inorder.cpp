00b374c240b0a16a9c41c67cd6d82fa65c19f0a9
ayushisahoo22 <ayushiis0912@gmail.com>
2026-06-27 21:51:08 +0530

    Morris Inorder Traversal

---
+++ binary_tree/morris_inorder.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node*left,*right;
+    Node(int d): data(d),left(nullptr),right(nullptr){}
+};
+
+vector<int> morrisInorder(Node*root){
+    vector<int>res;
+    if(!root) return res;
+    Node* curr=root;
+    while(curr){
+        if(curr->left==NULL){
+            res.push_back(curr->data);
+            curr=curr->right;
+        }else{
+            Node*prev=curr->left;
+            while(prev->right && prev->right!=curr) prev=prev->right;
+            if(prev->right==NULL){
+                prev->right=curr;
+                curr=curr->left;
+            }else{
+                prev->right=NULL;
+                res.push_back(curr->data);
+                curr=curr->right;
+            }
+        }
+    }
+    return res;
+}
+
+int main(){
+    Node*root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->left->right->right=new Node(6);
+
+    vector<int>res=morrisInorder(root);
+    for(auto it:res){
+        cout<<it<<" ";
+    }
+    return 0;
+}
